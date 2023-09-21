12c7a914f578f694f5b4c42d8967e09ad0076f34
sylwia <sekula.syl@gmail.com>
2023-09-21 17:47:10 +0200

    update

--- scripts/model/test_model.py
+++ scripts/model/test_model.py
@@ -1,31 +1,14 @@
 import pandas as pd
 import pickle
 import os
+from scripts.model.train_and_save_model import split_data
 from scripts.settings import *
 from sklearn.metrics import median_absolute_error, mean_absolute_percentage_error
 
-
-def split_data(dataframe: pd.DataFrame, target: str) -> (pd.DataFrame, pd.Series):
-    """
-    Split a dataset into feature and target variables.
-    :param dataframe: pd.Dataframe, the input DataFrame containing the dataset to be split.
-    :param target: str, the name of the target column in the DataFrame.
-    :return: tuple (pd.DataFrame, pd.Series), a tuple containing two components: X - pd.DataFrame - the feature
-    variables, which includes all columns except the target. y - pd.Series, the target variable, which is a Pandas
-    Series containing the values from the target column.
-    """
-    X = dataframe.drop(target, axis=1)  # features
-    y = dataframe[target]  # target
-    return X, y
-
-
 def main():
-    # load train and test datasets
-    df_train = pd.read_csv('../prepare_data/df_train.csv')
+    # load the test dataset
     df_test = pd.read_csv('../prepare_data/df_test.csv')
     target = 'price'
-    # create X and y for train and test data
-    X_train, y_train = split_data(df_train, target)
     X_test, y_test = split_data(df_test, target)
     # load the trained random forest model
     rf_regressor_opt = pickle.load(open(os.path.join(trained_model_dir, random_forest_file), 'rb'))
